9b851b9f658a3fb6e92c29f873f6e182b1b62512
Wbolgatz <93930065+Wbolgatz@users.noreply.github.com>
2021-11-15 15:30:25 +0000

    values idk

--- part1.py
+++ part1.py
@@ -20,6 +20,6 @@ The sum of the numbers you entered is 40
 #code starts here
 first_value = int(input("Enter a number: "))
 second_value = int(input("Enter a second number: "))
-third_value = int(input(" ")
-
+third_value = int(input("Enter a third number: "))
+fourth_value = int(input("Enter a fourth_value number: ")) 
 print("The sum of the numbers you entered is", first_value + second_value)
